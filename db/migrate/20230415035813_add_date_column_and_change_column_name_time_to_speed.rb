8b0a403e2ccd87d8eb0dcdb2dc171e904da73076
ShoKato-TeaBreak <katosho26@gmail.com>
2023-06-28 02:25:26 +0900

    Rewrite migration file for Heroku error resolution

--- db/migrate/20230415035813_add_date_column_and_change_column_name_time_to_speed.rb
+++ db/migrate/20230415035813_add_date_column_and_change_column_name_time_to_speed.rb
@@ -3,6 +3,10 @@ class AddDateColumnAndChangeColumnNameTimeToSpeed < ActiveRecord::Migration[7.0]
     add_column :workouts, :date, :date
     add_column :workouts, :speed, :integer
     remove_column :workouts, :time, :integer
-    remove_column :exercises, :time, :integer
+    remove_column_if_exists :exercises, :time, :integer
+  end
+
+  def remove_column_if_exists(table, column, type)
+    remove_column(table, column, type) if column_exists?(table, column)
   end
 end
