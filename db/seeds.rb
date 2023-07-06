aec0423882cc3db02ddc21274763ac27fb30ecd7
ShoKato-TeaBreak <katosho26@gmail.com>
2023-07-06 13:40:35 +0900

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Exercise.create!(
+    [
+      { name: "レッグプレス", mets_high: 8.0, mets_middle: 5.0, mets_low: 3.0 },
+      { name: "レッグエクステンション", mets_high: 6.0, mets_middle: 4.0, mets_low: 3.0 },
+      { name: "レッグカール", mets_high: 6.0, mets_middle: 4.0, mets_low: 3.0 },
+      { name: "チェストプレス", mets_high: 7.0, mets_middle: 5.0, mets_low: 3.0 },
+      { name: "ショルダープレス", mets_high: 7.0, mets_middle: 5.0, mets_low: 3.0 },
+      { name: "ラットプルダウン", mets_high: 6.0, mets_middle: 4.0, mets_low: 3.0 },
+      { name: "シーテッドロウ", mets_high: 6.0, mets_middle: 4.0, mets_low: 3.0 },
+      { name: "アブドミナル", mets_high: 5.0, mets_middle: 3.0, mets_low: 2.0 },
+      { name: "バックエクステンション", mets_high: 6.0, mets_middle: 4.0, mets_low: 2.0 },
+      { name: "バイセップカール", mets_high: 6.0, mets_middle: 4.5, mets_low: 3.0 },
+      { name: "ディップス", mets_high: 8.0, mets_middle: 6.0, mets_low: 4.0 },
+      { name: "アダクション", mets_high: 5.0, mets_middle: 3.5, mets_low: 2.5 },
+      { name: "アブダクション", mets_high: 5.0, mets_middle: 3.5, mets_low: 2.5 },
+    ]
+  )
